16a7ac5594e1675ee3070e5bbf03aa908b987196
Uche Nzei <uchenzei18@gmail.com>
2020-04-12 10:47:13 +0100

    Initial Setup

---
+++ src/main/java/com/drv/ait/RichInternetAssessment/RichInternetAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.drv.ait.RichInternetAssessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RichInternetAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RichInternetAssessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/drv/ait/RichInternetAssessment/RichInternetAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drv.ait.RichInternetAssessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RichInternetAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
